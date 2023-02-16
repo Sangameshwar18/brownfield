c7b98c538812bc6f16caf7afdd1251824525ff71
Sangameshwar18 <73937300+Sangameshwar18@users.noreply.github.com>
2023-02-16 11:09:00 +0530

    Update URL.js

--- src/api/URL.js
+++ src/api/URL.js
@@ -2,10 +2,10 @@
 const hostname = `LIN59017635`
 
 
-export const authURL = `http://${hostname}.corp.capgemini.com:8089/home`
-export const flightManagementURL = `http://${hostname}.corp.capgemini.com:8089/search`
-export const bookingManagementURL = `http://${hostname}.corp.capgemini.com:8089/booking`
-export const userDetailsURL = `http://${hostname}.corp.capgemini.com:8089/home`
+// export const authURL = `http://${hostname}.corp.capgemini.com:8089/home`
+// export const flightManagementURL = `http://${hostname}.corp.capgemini.com:8089/search`
+// export const bookingManagementURL = `http://${hostname}.corp.capgemini.com:8089/booking`
+// export const userDetailsURL = `http://${hostname}.corp.capgemini.com:8089/home`
 
 
 
@@ -14,8 +14,8 @@ export const userDetailsURL = `http://${hostname}.corp.capgemini.com:8089/home`
 // http://http://${hostname}:8089/home/getAllUser
 
 
-// export const authURL = `${process.env.REACT_APP_URL}/home`
-// export const flightManagementURL = `${process.env.REACT_APP_URL}/search`
-// export const bookingManagementURL = `${process.env.REACT_APP_URL}/booking`
-// export const userDetailsURL = `${process.env.REACT_APP_URL}/home`
+export const authURL = `${process.env.REACT_APP_URL}/home`
+export const flightManagementURL = `${process.env.REACT_APP_URL}/search`
+export const bookingManagementURL = `${process.env.REACT_APP_URL}/booking`
+export const userDetailsURL = `${process.env.REACT_APP_URL}/home`
 
